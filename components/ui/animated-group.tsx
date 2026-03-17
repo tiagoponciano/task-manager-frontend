0d9d274e308238fd2423ee39b7d11b431eb988c0
tiagoponciano <tiagoponciano03@gmail.com>
2026-03-17 14:42:47 -0300

    fix: update type specification for MotionComponent and MotionChild in AnimatedGroup

--- components/ui/animated-group.tsx
+++ components/ui/animated-group.tsx
@@ -116,11 +116,11 @@ function AnimatedGroup({
   const itemVariants = variants?.item || selectedVariants.item;
 
   const MotionComponent = React.useMemo(
-    () => motion.create(as as keyof JSX.IntrinsicElements),
+    () => motion.create(as as string),
     [as]
   );
   const MotionChild = React.useMemo(
-    () => motion.create(asChild as keyof JSX.IntrinsicElements),
+    () => motion.create(asChild as string),
     [asChild]
   );
 
